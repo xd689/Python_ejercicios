0ee12494e176a15ff3e4b98320f494f35e2b111f
Pedro <secundary9618@gmail.com>
2024-10-30 12:07:32 +0100

    Listas

---
+++ Listas1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def main(args):
+	a=['Filosofía','Física','Historia','Latín','Mates']
+	a.sort()
+	print(a)
+	return 0
+
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
--- Prueba.py
+++ Prueba.py
@@ -1,18 +1,31 @@
 def main(args):
-    n= int(input("Introduce un número entero positivo: "))
-    if n < 2:
-        es_primo = True
-    else:
-        for bcl in range(2, int(n*0.5)+1):
-           if n%bcl == 0:
-               es_primo = False
-               break
-    if es_primo:
-        print("El número es primo.")
-    else:
-        print("El número no es primo.")
+    def obten_mail(user):
+        email = user[3][0]  # Accede al correo completo
+        username = ""
+        for char in email:
+            if char != '@':
+                username += char
+            else:
+                return username
+
+    def obten_domain(user):
+        email = user[3][0]
+        domain = ""
+        encontrado = False
+        for char in email:
+            if encontrado:
+                domain += char
+            elif char == '@':
+                encontrado = True
+                domain += char
+        return domain
+
+    user = (("Diablos",), ("Desktop_12",), ("10.10.10.10",), ("diablos12@diablomail.com",))
+    username = obten_mail(user)
+    domain = obten_domain(user)
+    print("El email es:", username + domain)  # Muestra el correo completo
     return 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    sys.exit(main(sys.argv))
+    main(sys.argv)
