cc49fc0c6f4ac717fe09497125cafb5e614b035e
Pedro <secundary9618@gmail.com>
2024-11-27 11:30:05 +0100

    Dicc

---
+++ Diccionarios/Diccionario1.py
@@ -0,0 +1,49 @@
+def mostrarDatos(datos):
+    for servidor in datos:
+        print(servidor)
+    #
+    for servidor in datos:
+        print(f"{servidor}--->{datos[servidor]}")
+    #
+    for servidor in datos:
+        print(f"{servidor}")
+        for dato in datos[servidor]:
+            print(f"\t{dato}--->{datos[servidor][dato]}")
+    #
+    for servidor in datos:
+        print(f"{servidor}")
+        for clave,valor in datos[servidor].items():
+            if clave == 'DNS':
+                for a in valor:
+                    print(a)
+    #
+    for servidor in datos:
+        print(f"{servidor}")
+        for clave,valor in datos[servidor].items():
+            if clave != 'DNS':
+                print(f"\t{clave}--->{valor}")
+            else:
+                for a in valor:
+                    print(f"DNS--->{a}")        
+
+def main(args):
+    datos={
+        'Serv1':{
+            "ip":"192.168.8.1",
+            "mask":"255.255.255.0",
+            "gateway":"192.168.8.1",
+            "DNS":["1.1.1.1","8.8.8.8"]
+        },
+        'Serv2':{
+            "ip":"192.168.8.2",
+            "mask":"255.255.255.0",
+            "gateway":"192.168.8.1",
+            "DNS":["1.1.1.1","8.8.8.8"]
+    }        
+    }
+    mostrarDatos(datos)
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
